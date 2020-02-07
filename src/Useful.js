791c8e36f78e67d91cc1422ca1d1c7d774265c86
ISazhyna <isazhyna@gmail.com>
2020-02-07 19:32:26 +0200

    New component currency converter

---
+++ src/Useful.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Converter from "./component/Сonverter";
+
+class Useful extends Component {
+    render() { 
+        return <Converter/>;
+    }
+}
+ 
+export default Useful;
